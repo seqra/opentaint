1e2ddeb4f18ed331f9c4f8743827f88fccd2fd15
Daniil Logunov <43185213+DanielELog@users.noreply.github.com>
2026-04-09 19:58:40 +0300

    Fix original base filtering from alias group

--- core/opentaint-dataflow-core/opentaint-jvm-dataflow/src/main/kotlin/org/opentaint/dataflow/jvm/ap/ifds/JIRLocalAliasAnalysis.kt
+++ core/opentaint-dataflow-core/opentaint-jvm-dataflow/src/main/kotlin/org/opentaint/dataflow/jvm/ap/ifds/JIRLocalAliasAnalysis.kt
@@ -46,7 +46,7 @@ class JIRLocalAliasAnalysis(
         instIdx: Int, base: AccessPathBase.LocalVar
     ): List<AliasInfo>? =
         alias[instIdx]?.getOrDefault(base.idx, null)?.filter {
-            it !is AliasApInfo || it.accessors.isNotEmpty() || it.base != base
+            it !is AccessPathBase || it != base
         }?.map { it.wrapAliasInfo() }
 
     private fun getAccessPathBaseAliases(
@@ -54,7 +54,7 @@ class JIRLocalAliasAnalysis(
         instIdx: Int, base: AccessPathBase
     ): List<AliasInfo>? =
         alias[instIdx]?.getOrDefault(base, null)?.filter {
-            it !is AliasApInfo || it.accessors.isNotEmpty() || it.base != base
+            it !is AccessPathBase || it != base
         }?.map { it.wrapAliasInfo() }
 
     fun findMustAlias(base: AccessPathBase, statement: CommonInst): List<AliasInfo>? {
