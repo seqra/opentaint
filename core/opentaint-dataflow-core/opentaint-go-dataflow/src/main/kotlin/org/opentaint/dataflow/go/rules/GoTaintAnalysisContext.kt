83aa07d13947eb0c5b6b73511c642b8bf53c9b91
Valentyn Sobol <8640896+Saloed@users.noreply.github.com>
2026-05-25 17:16:09 +0300

    Go taint ctx

--- core/opentaint-dataflow-core/opentaint-go-dataflow/src/main/kotlin/org/opentaint/dataflow/go/rules/GoTaintAnalysisContext.kt
+++ core/opentaint-dataflow-core/opentaint-go-dataflow/src/main/kotlin/org/opentaint/dataflow/go/rules/GoTaintAnalysisContext.kt
@@ -1,10 +1,11 @@
 package org.opentaint.dataflow.go.rules
 
+import org.opentaint.dataflow.ap.ifds.taint.ExternalMethodTracker
 import org.opentaint.dataflow.ap.ifds.taint.TaintAnalysisContext
 import org.opentaint.dataflow.ap.ifds.taint.TaintSinkTracker
-import org.opentaint.dataflow.configuration.CommonTaintRulesProvider
 
 class GoTaintAnalysisContext(
     override val taintSinkTracker: TaintSinkTracker,
     val taintConfig: GoTaintRulesProvider,
+    val externalMethodTracker: ExternalMethodTracker? = null,
 ): TaintAnalysisContext
