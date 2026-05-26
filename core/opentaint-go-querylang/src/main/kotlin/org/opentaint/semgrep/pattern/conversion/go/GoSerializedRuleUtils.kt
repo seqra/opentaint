23df9d6571b4df93de6393079dfa23f6148c7d1d
Valentyn Sobol <8640896+Saloed@users.noreply.github.com>
2026-05-26 12:13:59 +0300

    Rule utils

---
+++ core/opentaint-go-querylang/src/main/kotlin/org/opentaint/semgrep/pattern/conversion/go/GoSerializedRuleUtils.kt
@@ -0,0 +1,25 @@
+package org.opentaint.semgrep.pattern.conversion.go
+
+import org.opentaint.dataflow.configuration.go.serialized.GoNameMatcher
+import org.opentaint.dataflow.configuration.go.serialized.GoSerializedAssignAction
+import org.opentaint.dataflow.configuration.go.serialized.GoSerializedCleanAction
+import org.opentaint.dataflow.configuration.go.serialized.GoSerializedCondition
+import org.opentaint.dataflow.configuration.jvm.serialized.PositionBase
+import org.opentaint.dataflow.configuration.jvm.serialized.PositionBaseWithModifiers
+import org.opentaint.semgrep.pattern.Mark.GeneratedMark
+
+internal fun PositionBase.baseGo(): PositionBaseWithModifiers = PositionBaseWithModifiers.BaseOnly(this)
+
+internal fun goAnyFunction(): GoNameMatcher = GoNameMatcher.Pattern(".*")
+
+internal fun GoNameMatcher.matchAnything(): Boolean =
+    this is GoNameMatcher.Pattern && regex == ".*"
+
+internal fun GeneratedMark.mkGoContainsMark(pos: PositionBaseWithModifiers): GoSerializedCondition.ContainsMark =
+    GoSerializedCondition.ContainsMark(taintMarkStr(), pos)
+
+internal fun GeneratedMark.mkGoAssignMark(pos: PositionBaseWithModifiers): GoSerializedAssignAction =
+    GoSerializedAssignAction(taintMarkStr(), pos)
+
+internal fun GeneratedMark.mkGoCleanMark(pos: PositionBaseWithModifiers): GoSerializedCleanAction =
+    GoSerializedCleanAction(taintMarkStr(), pos)
