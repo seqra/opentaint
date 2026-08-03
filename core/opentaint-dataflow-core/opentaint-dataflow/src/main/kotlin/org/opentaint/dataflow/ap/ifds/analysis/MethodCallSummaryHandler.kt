f7b3b720a7da4329fe145f9631a2484242f8d439
Valentyn Sobol <8640896+Saloed@users.noreply.github.com>
2026-08-03 23:28:09 +0300

    Edge refinement

--- core/opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/ap/ifds/analysis/MethodCallSummaryHandler.kt
+++ core/opentaint-dataflow-core/opentaint-dataflow/src/main/kotlin/org/opentaint/dataflow/ap/ifds/analysis/MethodCallSummaryHandler.kt
@@ -3,6 +3,7 @@ package org.opentaint.dataflow.ap.ifds.analysis
 import org.opentaint.dataflow.ap.ifds.Edge
 import org.opentaint.dataflow.ap.ifds.ExclusionSet
 import org.opentaint.dataflow.ap.ifds.FactTypeChecker
+import org.opentaint.dataflow.ap.ifds.MethodSummaryEdgeApplicationUtils.EdgeRefinement
 import org.opentaint.dataflow.ap.ifds.MethodSummaryEdgeApplicationUtils.SummaryEdgeApplication
 import org.opentaint.dataflow.ap.ifds.MethodSummaryEdgeApplicationUtils.SummaryEdgeApplication.SummaryApRefinement
 import org.opentaint.dataflow.ap.ifds.MethodSummaryEdgeApplicationUtils.SummaryEdgeApplication.SummaryExclusionRefinement
@@ -34,7 +35,7 @@ interface MethodCallSummaryHandler {
 
     fun handleZeroToFact(
         currentFactAp: FinalFactAp,
-        summaryEffect: SummaryEdgeApplication,
+        summaryEffect: EdgeRefinement,
         summaryEdge: SummaryEdge,
     ): Set<Sequent> = handleSummary(
         currentFactAp,
@@ -55,7 +56,7 @@ interface MethodCallSummaryHandler {
     fun handleFactToFact(
         initialFactAp: InitialFactAp,
         currentFactAp: FinalFactAp,
-        summaryEffect: SummaryEdgeApplication,
+        summaryEffect: EdgeRefinement,
         summaryEdge: SummaryEdge,
     ): Set<Sequent> = handleSummary(
         currentFactAp,
@@ -73,7 +74,7 @@ interface MethodCallSummaryHandler {
     fun handleNDFactToFact(
         initialFacts: Set<InitialFactAp>,
         currentFactAp: FinalFactAp,
-        summaryEffect: SummaryEdgeApplication,
+        summaryEffect: EdgeRefinement,
         summaryEdge: SummaryEdge,
     ): Set<Sequent> = handleSummary(
         currentFactAp,
@@ -102,7 +103,7 @@ interface MethodCallSummaryHandler {
 
     fun handleSummary(
         currentFactAp: FinalFactAp,
-        summaryEffect: SummaryEdgeApplication,
+        summaryEffect: EdgeRefinement,
         summaryEdge: SummaryEdge,
         createSideEffectRequirement: (refinement: ExclusionSet) -> Sequent?,
         handleSummaryEdge: (initialFactRefinement: ExclusionSet?, summaryFactAp: FinalFactAp) -> Sequent
@@ -110,21 +111,31 @@ interface MethodCallSummaryHandler {
         val mappedSummaryFacts = mapMethodExitToReturnFlowFact(summaryEdge.final)
 
         return when (summaryEffect) {
-            is SummaryApRefinement -> mappedSummaryFacts.mapNotNullTo(hashSetOf()) { mappedSummaryFact ->
-                // todo: filter exclusions
+            is SummaryEdgeApplication -> mappedSummaryFacts.mapNotNullTo(hashSetOf()) { mappedSummaryFact ->
                 val summaryFactAp = mappedSummaryFact
                     .concat(factTypeChecker, summaryEffect.delta)
-                    ?.replaceExclusions(currentFactAp.exclusions)
                     ?: return@mapNotNullTo null
 
-                handleSummaryEdge(null, summaryFactAp)
+                when (summaryEffect) {
+                    is SummaryApRefinement -> {
+                        // todo: filter exclusions
+                        val fact = summaryFactAp.replaceExclusions(currentFactAp.exclusions)
+                        handleSummaryEdge(null, fact)
+                    }
+
+                    is SummaryExclusionRefinement -> {
+                        val fact = summaryFactAp.replaceExclusions(summaryEffect.exclusion)
+                        handleSummaryEdge(summaryEffect.exclusion, fact)
+                    }
+                }
             }
 
-            is SummaryExclusionRefinement -> mappedSummaryFacts.mapTo(hashSetOf()) { mappedSummaryFact ->
-                // todo: filter exclusions
-                val summaryFactAp = mappedSummaryFact.replaceExclusions(summaryEffect.exclusion)
+            is EdgeRefinement.UniverseRefinement -> mappedSummaryFacts.mapTo(hashSetOf()) {
+                handleSummaryEdge(ExclusionSet.Universe, it.replaceExclusions(ExclusionSet.Universe))
+            }
 
-                handleSummaryEdge(summaryEffect.exclusion, summaryFactAp)
+            is EdgeRefinement.IdRefinement -> mappedSummaryFacts.mapTo(hashSetOf()) {
+                handleSummaryEdge(currentFactAp.exclusions, it.replaceExclusions(currentFactAp.exclusions))
             }
         }
     }
