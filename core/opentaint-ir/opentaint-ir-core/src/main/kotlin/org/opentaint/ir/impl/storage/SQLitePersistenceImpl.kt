38fa970f3d9567c43e4cd004e3a36bb735132f74
Valentyn Sobol <8640896+Saloed@users.noreply.github.com>
2026-08-15 00:57:57 +0300

    remove unsued single param

--- core/opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/storage/SQLitePersistenceImpl.kt
+++ core/opentaint-ir/opentaint-ir-core/src/main/kotlin/org/opentaint/ir/impl/storage/SQLitePersistenceImpl.kt
@@ -108,16 +108,13 @@ class SQLitePersistenceImpl(
             return CLASSES.LOCATION_ID.`in`(registeredLocationIds)
         }
 
-    private fun JIRDatabase.classSources(clause: Condition, single: Boolean = false): List<ClassSource> =
+    private fun JIRDatabase.classSources(clause: Condition): List<ClassSource> =
         read { context ->
             val jooq = context.dslContext
             val classesQuery =
                 jooq.select(CLASSES.LOCATION_ID, CLASSES.ID, CLASSES.BYTECODE, SYMBOLS.NAME).from(CLASSES).join(SYMBOLS)
                     .on(CLASSES.NAME.eq(SYMBOLS.ID)).where(clause)
-            val classes = when {
-                single -> listOfNotNull(classesQuery.fetchAny())
-                else -> classesQuery.fetch()
-            }
+            val classes = classesQuery.fetch()
             classes.map { (locationId, classId, bytecode, name) ->
                 PersistenceClassSource(
                     db = this,
